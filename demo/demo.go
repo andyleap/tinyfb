b53addf6a62eb39baa80047faee5db7031f34f3a
Andrew Leap <andyleap@gmail.com>
2018-12-27 09:49:51 -0800

    initial mac support

--- demo/demo.go
+++ demo/demo.go
@@ -18,54 +18,55 @@ func main() {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	quit := false
 	go func() {
-		t.Run()
-		quit = true
-	}()
-	perlin := noisey.NewOpenSimplexGenerator(r)
+		time.Sleep(1 * time.Second)
 
-	i := image.NewRGBA(image.Rect(0, 0, 400, 400))
-	step := 0
-	xstep := 0
-	ystep := 0
-	frame := time.Now()
+		perlin := noisey.NewOpenSimplexGenerator(r)
 
-	t.Char(func(char string, mods int) {
-		switch char {
-		case "Up":
-			ystep -= 10
-		case "Down":
-			ystep += 10
-		case "Left":
-			xstep -= 10
-		case "Right":
-			xstep += 10
-		default:
-			log.Println(char, mods)
+		i := image.NewRGBA(image.Rect(0, 0, 400, 400))
+		step := 0
+		xstep := 0
+		ystep := 0
+		frame := time.Now()
 
-		}
-	})
+		t.Char(func(char string, mods int) {
+			switch char {
+			case "Up":
+				ystep -= 10
+			case "Down":
+				ystep += 10
+			case "Left":
+				xstep -= 10
+			case "Right":
+				xstep += 10
+			default:
+				log.Println(char, mods)
 
-	for !quit {
-		for x := 0; x <= 400; x++ {
-			for y := 0; y <= 400; y++ {
-				noise := perlin.Get3D(float64(x+xstep)*0.03, float64(y+ystep)*0.03, float64(step)*0.01)
-				val := uint8(math.Floor((noise*0.4 + 0.5) * 250))
-				i.SetRGBA(x, y, color.RGBA{
-					R: val,
-					G: val,
-					B: val,
-					A: 0,
-				})
 			}
-		}
-		t.Update(i)
-		step += 1
-		end := time.Now()
-		delta := end.Sub(frame).Nanoseconds() - (time.Second / 60).Nanoseconds()
-		if delta < 0 {
-			time.Sleep(time.Duration(-delta))
-		}
-		frame = time.Now()
-	}
+		})
 
+		for !quit {
+			for x := 0; x <= 400; x++ {
+				for y := 0; y <= 400; y++ {
+					noise := perlin.Get3D(float64(x+xstep)*0.03, float64(y+ystep)*0.03, float64(step)*0.01)
+					val := uint8(math.Floor((noise*0.4 + 0.5) * 250))
+					i.SetRGBA(x, y, color.RGBA{
+						R: val,
+						G: val,
+						B: val,
+						A: 0,
+					})
+				}
+			}
+			t.Update(i)
+			step += 1
+			end := time.Now()
+			delta := end.Sub(frame).Nanoseconds() - (time.Second / 60).Nanoseconds()
+			if delta < 0 {
+				time.Sleep(time.Duration(-delta))
+			}
+			frame = time.Now()
+		}
+	}()
+	t.Run()
+	quit = true
 }
---
+++ tinyfb_mac.go
@@ -0,0 +1,78 @@
+package tinyfb
+
+import (
+	"image"
+	"image/draw"
+
+//	"golang.org/x/mobile/event/key"
+//	"golang.org/x/mobile/event/mouse"
+
+	"golang.org/x/exp/shiny/driver"
+	"golang.org/x/exp/shiny/screen"
+)
+
+type tinyFB struct {
+	title         string
+	width, height int
+
+	w screen.Window
+
+	char func(char string, mods int)
+	key  func(key string, mods int, press bool)
+}
+
+func New(title string, width, height int32) TinyFB {
+	return &tinyFB{
+		title:  title,
+		width:  int(width),
+		height: int(height),
+	}
+}
+
+func (t *tinyFB) Run() {
+	driver.Main(func(s screen.Screen) {
+		w, err := s.NewWindow(&screen.NewWindowOptions{Title: t.title, Width: t.width, Height: t.height})
+		t.w = w
+		if err != nil {
+			panic(err)
+			return
+		}
+		defer w.Release()
+
+		buf, _ := s.NewBuffer(image.Point{t.width, t.height})
+		tex, _ := s.NewTexture(image.Point{t.width, t.height})
+
+		for {
+			e := w.NextEvent()
+
+			switch et := e.(type) {
+			case *image.RGBA:
+				draw.Draw(buf.RGBA(), buf.Bounds(), et, image.Point{}, draw.Src)
+
+				tex.Upload(image.Point{}, buf, buf.Bounds())
+				w.Copy(image.Point{}, tex, tex.Bounds(), screen.Src, nil)
+				w.Publish()
+			case string:
+				if et == "quit" {
+					return
+				}
+			}
+		}
+	})
+}
+
+func (t *tinyFB) Update(buffer *image.RGBA) {
+	t.w.Send(buffer)
+}
+
+func (t *tinyFB) Close() {
+	t.w.Send("quit")
+}
+
+func (t *tinyFB) Char(char func(char string, mods int)) {
+	t.char = char
+}
+
+func (t *tinyFB) Key(key func(key string, mods int, press bool)) {
+	t.key = key
+}
